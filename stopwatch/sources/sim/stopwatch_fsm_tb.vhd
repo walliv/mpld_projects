fcd01b5b814dad2a40b2dd4b4b087e987546f12b
Vladislav Valek <vladislav.valek@email.cz>
2022-03-21 22:09:12 +0100

    stopwatch_fsm_tb [BUGFIX]: some major bugfixes
    I performed not an easy debug, let's call it defuck. Firstly I added the
    indication if simulation is finished. Second I removed the "wait until
    rising_edge(clk)" because for some reason Modlesim makes bystandig
    signals really small (like tiny tiny small) and Vivado simulator does
    not react on them at all.

--- stopwatch/sources/sim/stopwatch_fsm_tb.vhd
+++ stopwatch/sources/sim/stopwatch_fsm_tb.vhd
@@ -7,6 +7,8 @@ end entity;
 
 architecture BEHAVIORAL of STOPWATCH_FSM_TB is
 
+    signal simulation_finished : boolean := FALSE;
+
     signal clk : std_logic;
     signal rst : std_logic;
 
@@ -16,7 +18,7 @@ architecture BEHAVIORAL of STOPWATCH_FSM_TB is
     signal cnt_enable  : std_logic;
     signal disp_enable : std_logic;
 
-    constant CLK_PERIOD : positive := 20 NS;
+    constant CLK_PERIOD : time := 20 NS;
 
 begin
 
@@ -31,12 +33,16 @@ begin
             DISP_ENABLE => disp_enable
             );
 
-    lk_p : process
+    clk_p : process
     begin
         clk <= '0';
         wait for CLK_PERIOD/2;
         clk <= '1';
         wait for CLK_PERIOD/2;
+
+        if simulation_finished then
+            wait;
+        end if;
     end process;  -- lk_p
 
     stim_p : process
@@ -45,88 +51,90 @@ begin
         btn_start <= '0';
         btn_lap   <= '0';
 
-        wait for 100 NS;
+        wait for CLK_PERIOD*10;
 
         rst <= '0';
-        wait for 30 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition IDLE -> RUN
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition RUN -> CNTR_STOP
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition CNTR_STOP -> RUN
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition RUN -> CNTR_STOP
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition CNTR_STOP -> IDLE
         btn_lap <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_lap <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
-        -- transition IDLE -> RUN 
+        -- transition IDLE -> RUN
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition RUN -> LAP
         btn_lap <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_lap <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition LAP -> REFRESH
         btn_lap <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_lap <= '0';
 
         -- unconditional transition back to the LAP
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition LAP -> RUN
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 40 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition RUN -> CNTR_STOP
         btn_start <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_start <= '0';
 
-        wait for 20 NS;
+        wait for CLK_PERIOD*10;
 
         -- transition CNTR_STOP -> IDLE
         btn_lap <= '1';
-        wait until rising_edge(clk);
+        wait for CLK_PERIOD;
         btn_lap <= '0';
 
+        wait for clk_period * 5;
+        simulation_finished <= TRUE;
         wait;
     end process;  -- stim_p
 
