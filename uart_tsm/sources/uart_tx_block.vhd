c40e5276d8e0de2534c173af05f4202c88a3c058
Vladislav Valek <vladislav.valek@email.cz>
2022-03-27 18:50:04 +0200

    [MAINTENANCE]: various code beautifications

--- uart_tsm/sources/uart_tx_block.vhd
+++ uart_tsm/sources/uart_tx_block.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 ----------------------------------------------------------------------------------
-ENTITY uart_tx_block IS
-    PORT(
-      CLK                 : IN  STD_LOGIC;
-      UART_TX_START       : IN  STD_LOGIC;
-      UART_CLK_EN         : IN  STD_LOGIC;
-      UART_DATA_IN        : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-      UART_TX_BUSY        : OUT STD_LOGIC;
-      UART_TX_DATA_OUT    : OUT STD_LOGIC
-    );
-END uart_tx_block;
+entity UART_TX_BLOCK is
+    port(
+        CLK              : in  std_logic;
+        UART_TX_START    : in  std_logic;
+        UART_CLK_EN      : in  std_logic;
+        UART_DATA_IN     : in  std_logic_vector(7 downto 0);
+        UART_TX_BUSY     : out std_logic;
+        UART_TX_DATA_OUT : out std_logic
+        );
+end entity;
 ----------------------------------------------------------------------------------
-ARCHITECTURE Behavioral OF uart_tx_block IS
+architecture FULL of UART_TX_BLOCK is
 ----------------------------------------------------------------------------------
 
     type uart_tx_state_type is (IDLE, WAIT_EN, ST_START_B, ST_STOP_0);
@@ -22,15 +22,15 @@ ARCHITECTURE Behavioral OF uart_tx_block IS
     signal swch_nst : swch_state_type := IDLE;
 
 ----------------------------------------------------------------------------------
-BEGIN
+begin
 ----------------------------------------------------------------------------------
 
-    uart_tx_fsm_pst_r : process( clk )
+    uart_tx_fsm_pst_r : process(clk)
     begin
         if (rising_edge(clk)) then
             swch_pst <= swch_nst;
         end if;
-    end process; 
+    end process;
 
     uart_nst_logic : process (all)
     begin
@@ -38,7 +38,7 @@ BEGIN
             when IDLE =>
 
                 if (UART_TX_START = '1') then
-                    swch_nst <= WAIT_EN;
+                    swch_nst     <= WAIT_EN;
                     UART_TX_BUSY <= '1';
                 end if;
 
@@ -47,13 +47,13 @@ BEGIN
                 if (UART_CLK_EN = '1') then
 
 
-                    
+
                 end if;
 
             when ST_START_B =>
 
             when ST_STOP_0 =>
-            
+
         end case;
 
     end process;
@@ -61,5 +61,5 @@ BEGIN
 
 
 ----------------------------------------------------------------------------------
-END architecture;
+end architecture;
 ----------------------------------------------------------------------------------
--- uart_tsm/sources/uart_tx_block_tb.vhd
+++ uart_tsm/sources/uart_tx_block_tb.vhd
@@ -1,108 +1,108 @@
 ------------------------------------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 ------------------------------------------------------------------------------------------------------------
 entity UART_Tx_block_tb is
 end UART_Tx_block_tb;
 ------------------------------------------------------------------------------------------------------------
-architecture Behavioral of UART_Tx_block_tb is
+architecture BEHAVIORAL of UART_TX_BLOCK_TB is
 ------------------------------------------------------------------------------------------------------------
 
-  COMPONENT UART_Tx_block
-  PORT(
-    clk                 : IN  STD_LOGIC;
-    UART_Tx_start       : IN  STD_LOGIC;
-    UART_clk_EN         : IN  STD_LOGIC;
-    UART_Data_in        : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-    UART_Tx_busy        : OUT STD_LOGIC;
-    UART_Tx_Data_out    : OUT STD_LOGIC
-  );
-  END COMPONENT;
+    component UART_TX_BLOCK
+        port(
+            CLK              : in  std_logic;
+            UART_TX_START    : in  std_logic;
+            UART_CLK_EN      : in  std_logic;
+            UART_DATA_IN     : in  std_logic_vector(7 downto 0);
+            UART_TX_BUSY     : out std_logic;
+            UART_TX_DATA_OUT : out std_logic
+            );
+    end component;
 
 ------------------------------------------------------------------------------------------------------------
 
-  component ce_gen
-  generic (
-    DIV_FACT            : positive := 2
-  );
-  port (
-    clk                 : in  std_logic;
-    srst                : in  std_logic;
-    ce                  : in  std_logic;
-    ce_o                : out std_logic
-  );
-  end component;
+    component CE_GEN
+        generic (
+            DIV_FACT : positive := 2
+            );
+        port (
+            CLK  : in  std_logic;
+            SRST : in  std_logic;
+            CE   : in  std_logic;
+            CE_O : out std_logic
+            );
+    end component;
 
-  ----------------------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------------------
 
-  CONSTANT clk_period      : TIME := 20 ns;
-  SIGNAL clk               : STD_LOGIC := '0';
+    constant clk_period : time      := 20 NS;
+    signal clk          : std_logic := '0';
 
-  -- UUT inputs
-  SIGNAL UART_Tx_start     : STD_LOGIC := '0';
-  SIGNAL UART_Data_In      : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL UART_clk_EN       : STD_LOGIC;
+    -- UUT inputs
+    signal uart_tx_start : std_logic                    := '0';
+    signal uart_data_in  : std_logic_vector(7 downto 0) := (others => '0');
+    signal uart_clk_en   : std_logic;
 
-  -- UUT outputs
-  SIGNAL UART_Tx_Data_out  : STD_LOGIC;
-  SIGNAL UART_Tx_busy      : STD_LOGIC;
+    -- UUT outputs
+    signal uart_tx_data_out : std_logic;
+    signal uart_tx_busy     : std_logic;
 
 
 ------------------------------------------------------------------------------------------------------------
 begin
 ------------------------------------------------------------------------------------------------------------
 
-  -- clk_gen
-  clk_gen: PROCESS
-  BEGIN
-    clk <= '0';
-    WAIT FOR clk_period/2;
-    clk <= '1';
-    WAIT FOR clk_period/2;
-  END PROCESS;
+    -- clk_gen
+    clk_gen : process
+    begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+    end process;
 
-  ----------------------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------------------
 
-  -- UART_Tx_block
-  UART_Tx_block_i : UART_Tx_block
-  PORT MAP(
-    clk                 => CLK,
-    UART_Tx_start       => UART_Tx_start,
-    UART_clk_EN         => UART_clk_EN,
-    UART_Data_In        => UART_Data_In,
-    UART_Tx_Data_out    => UART_Tx_Data_out,
-    UART_Tx_busy        => UART_Tx_busy
-  );
+    -- UART_Tx_block
+    uart_tx_block_i : UART_TX_BLOCK
+        port map(
+            clk              => CLK,
+            UART_Tx_start    => UART_Tx_start,
+            UART_clk_EN      => UART_clk_EN,
+            UART_Data_In     => UART_Data_In,
+            UART_Tx_Data_out => UART_Tx_Data_out,
+            UART_Tx_busy     => UART_Tx_busy
+            );
 
-  ----------------------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------------------
 
-  -- clock enable generator
-  ce_gen_i : ce_gen
-  GENERIC MAP(
-    DIV_FACT            => 20
-  )
-  PORT MAP(
-    clk                 => CLK,
-    srst                => '0',
-    ce                  => '1',
-    ce_o                => UART_clk_EN
-  );
+    -- clock enable generator
+    ce_gen_i : CE_GEN
+        generic map(
+            DIV_FACT => 20
+            )
+        port map(
+            clk  => CLK,
+            srst => '0',
+            ce   => '1',
+            ce_o => UART_clk_EN
+            );
 
-  ----------------------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------------------
 
-  UART_Tx_stimuli: PROCESS
-  BEGIN
+    stim_p : process
+    begin
 
-    UART_Data_In  <= X"00";
-    UART_Tx_start <= '0';
-    WAIT FOR clk_period * 300;
+        UART_Data_In  <= X"00";
+        UART_Tx_start <= '0';
+        wait for clk_period * 300;
 
 
 
-    REPORT "================================================================" SEVERITY NOTE;
-    REPORT "Simulation finished!" SEVERITY NOTE;
-    REPORT "================================================================" SEVERITY FAILURE;
-  END PROCESS;
+        report "================================================================" severity NOTE;
+        report "Simulation finished!" severity NOTE;
+        report "================================================================" severity FAILURE;
+    end process;
 
 ---------------------------------------------------------------------------------
 end Behavioral;
